44c49045e3253ef1d67ab9f3c7fecd0121961716
ryoma okuda <catontheskate4423@gmail.com>
2021-05-07 11:31:41 +0900

    unko

---
+++ pi/ex5/facedetect.py
@@ -0,0 +1,9 @@
+import cv2
+faceCascade = cv2.CascadeClassifier('.opencv-master/data/haarcascades/haarcascades_ftontalface_default.xml')
+
+img = cv2.imread('face.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+faces = faceCascade.detectMultiScale(gray)
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+cv2.imwrite('detected.jpg', img)
