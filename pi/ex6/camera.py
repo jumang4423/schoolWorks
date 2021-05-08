d713e0a825b8a574fa825727548cf42bcb9fe091
jumang4423 <catontheskate4423@gmail.com>
2021-05-08 22:25:18 +0900

    pis

---
+++ pi/ex6/camera.py
@@ -0,0 +1,20 @@
+#coding: utf-8
+import webiopi
+import os
+import datetime
+
+#save directory
+SAVEDIR = '/home/pi/ex6'
+@webiopi.macro
+def camera(no):
+    #filename
+    filename = SAVEDIR + '/camera_' + no + '.jpg'
+    #taking a photo
+    command = 'fswebcam -r 320x240 -d /dev/video0 ' + filename
+    os.system(command)
+    #writing to disk cache
+    os.system('sync')
+    
+ #camera('1')
+
+
