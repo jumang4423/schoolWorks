8dfc19b331985c815c46c4186fd107f6b35c86f4
ryoma okuda <catontheskate4423@gmail.com>
2021-05-27 02:56:22 +0900

    fu04java

--- fu04/se1ex12/se1ex12/src/ProcurementControl.java
+++ fu04/se1ex12/se1ex12/src/ProcurementControl.java
@@ -5,9 +5,9 @@ package se1ex12.src;
  *
  * Class: ProcurementControl
  *
- * Date: 
+ * Date: 2021/05/26
  *
- * author: 
+ * author: Ryoma Okuda
  *
  */
 /*
@@ -20,7 +20,7 @@ import java.util.Date;
 
 public class ProcurementControl {
   // Declare necessary variables
-  private Procurement procurement;
+  // private Procurement procurement;
   private DBAccess dbAccess;
 
   public ProcurementControl() throws ClassNotFoundException, SQLException {
@@ -29,5 +29,54 @@ public class ProcurementControl {
   }
 
   // Declare and implement necessary methods
+
+  // return one ProcurementStaff object
+  // - null for non staff
+  public ProcurementStaff getProcurementStaff(String _staffCode) {
+    // new instance
+    ProcurementStaff procurementStaff = new ProcurementStaff(_staffCode, this.dbAccess);
+
+    return procurementStaff.checkStaff() ? procurementStaff : null;
+  }
+
+  // return one Material object
+  // - null for non staff
+  public Material getMaterial(String _materialCode) {
+    // new instance
+    Material material = new Material(_materialCode, this.dbAccess);
+
+    return material.checkMaterial() ? material : null;
+  }
+
+  // return one Procurement object
+  // - null for non staff
+  public Procurement getProcurement(String _procurementId) {
+    // new instance
+    Procurement procurement = new Procurement(_procurementId, this.dbAccess);
+
+    return procurement.checkProcurement() ? procurement : null;
+  }
+
+  // return boolean that added procurement exists or not
+  // - null for non procurement
+  public Boolean recordProcurement(String _procurementId, String _staffCode, String _materialCode, Integer _amount,
+      Date _requestedDate) {
+    Procurement procurement = new Procurement(_procurementId, _staffCode, _materialCode, _amount, _requestedDate,
+        this.dbAccess);
+    return procurement.addProcurement();
+  }
+
+  // return boolean that setDeliveryCompleted() result
+  // - null for non procurement
+  public boolean recordDelivery(String _procurementId, Date _deliveryDate) {
+    Procurement procurement = new Procurement(_procurementId, this.dbAccess);
+
+    if (procurement.checkProcurement()) {
+      procurement.setDeliveryDate(_deliveryDate);
+      return procurement.setDeliveryCompleted();
+    } else {
+      return false;
+    }
+  }
 
 }
--- fu04/se1ex12/se1ex12/src/RecordProcurementPanel.java
+++ fu04/se1ex12/se1ex12/src/RecordProcurementPanel.java
@@ -4,9 +4,9 @@ package se1ex12.src;
  *
  * Class: RecordProcurementPanel
  *
- * Date: 
+ * Date: 2021/5/26
  *
- * author: 
+ * author: Ryoma Okuda
  * 
  */
 
@@ -27,6 +27,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
+import java.util.Objects;
 
 public class RecordProcurementPanel implements ActionListener {
   // Controls for values
@@ -182,36 +183,53 @@ public class RecordProcurementPanel implements ActionListener {
   }
 
   // Implement the following method "recordProcurement"
-  private void recordProcurement(String procurementId, String staffCode, String materialCode,
-      Integer amount, Date requestedDate) throws ClassNotFoundException, SQLException {
+  private void recordProcurement(String procurementId, String staffCode, String materialCode, Integer amount,
+      Date requestedDate) throws ClassNotFoundException, SQLException {
     // Define necessary variables
     ProcurementControl procurementControl = new ProcurementControl();
-   
 
-    boolean status = false;
-    String processStatusMessage=null;
+    // boolean status = false;
+    String processStatusMessage = null;
     String staffName = null;
     String materialName = null;
 
     // Main process
 
+    ProcurementStaff staff = procurementControl.getProcurementStaff(staffCode);
+    Material material = procurementControl.getMaterial(materialCode);
+
+    // what if staff and material objects isnt null
+    if (!Objects.isNull(staff)) {
+      staffName = staff.getName();
+    }
+    if (!Objects.isNull(material)) {
+      materialName = material.getName();
+    }
+
     // Finally, call method "show"
     show(staffName, materialName, processStatusMessage);
   }
 
-
   // Implement the following method "show"
   private void show(String staffName, String materialName, String processStatusMessage) {
 
-    // Check if staffName is empty, if not set it to staffNameLabel, if empty set error message
+    // Check if staffName is empty, if not set it to staffNameLabel, if empty set
+    if (staffName != null && !staffName.equals("")) {
+      staffNameLabel.setText(staffName);
+    } else {
+      // error message
+      staffCodeError.setText("Staff Code is invaild");
+    }
 
-
-    // Check if materialName is empty, if not, set it to materialNameLabel, if empty set error
-    // message
-
-
+    // Check if materialName is empty, if not, set it to materialNameLabel, if empty
+    if (materialName != null && !materialName.equals("")) {
+      materialNameLabel.setText(materialName);
+    } else {
+      // error message
+      materialCodeError.setText("Material Code is invaild");
+    }
 
     // Set the processStatusMessage
-  
+    processStatusMessageLabel.setText(processStatusMessage);
   }
 }
