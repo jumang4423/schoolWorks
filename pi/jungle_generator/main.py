26fd6f867a6a5131d2a508efa90eab2b6381b71e
ryoma okuda <catontheskate4423@gmail.com>
2021-06-19 13:49:33 +0900

    debug

--- pi/jungle_generator/main.py
+++ pi/jungle_generator/main.py
@@ -2,6 +2,7 @@
 from pydub import AudioSegment
 from pydub.playback import play
 import simpleaudio
+import time
 
 
 def seqToBuffer(sequences, sounds):
@@ -54,28 +55,34 @@ def main():
         {"id": 2, "vol": 1, "pan": 0, "speed": 1, "reverse": False},
         {"id": 3, "vol": 1, "pan": 0, "speed": 1, "reverse": True},
         {"id": 2, "vol": 1, "pan": 0, "speed": 1, "reverse": False},
-        {"id": 0, "vol": 1, "pan": 0, "speed": 1, "reverse": False},
-        {"id": 7, "vol": 1, "pan": 0, "speed": 1, "reverse": False},
-        {"id": 2, "vol": 1, "pan": 0, "speed": 1, "reverse": False},
-        {"id": 3, "vol": 1, "pan": 0, "speed": 1, "reverse": False},
-        {"id": 1, "vol": 1, "pan": 0, "speed": 1, "reverse": False},
-        {"id": 2, "vol": 1, "pan": 0, "speed": 1, "reverse": False},
-        {"id": 3, "vol": 1, "pan": 0, "speed": 1, "reverse": True},
-        {"id": 2, "vol": 1, "pan": 0, "speed": 1, "reverse": False},
     ]
 
-    record = AudioSegment.empty()
-    # while True:
-    _playlist = seqToBuffer(sequences, sounds)
-    record += _playlist
-    # musicLoop(_playlist)
-    play_obj = simpleaudio.play_buffer(
-        _playlist.raw_data,
-        num_channels=_playlist.channels,
-        bytes_per_sample=_playlist.sample_width,
-        sample_rate=_playlist.frame_rate,
-    )
-    play_obj.wait_done()
+    record = AudioSegment.empty() # recorder
+
+    timed = 0
+    _from_sec = time.time()
+    delay_time = 0
+    while True:
+        _playlist = seqToBuffer(sequences, sounds)
+        record += _playlist
+        _delay_start = time.time()
+        play_obj = simpleaudio.play_buffer(
+            _playlist.raw_data,
+            num_channels=_playlist.channels,
+            bytes_per_sample=_playlist.sample_width,
+            sample_rate=_playlist.frame_rate,
+        )
+
+        if delay_time == 0:
+            delay_time = time.time() - _delay_start
+
+        if timed != 0:
+            time.sleep(timed - delay_time)
+
+        if timed == 0:
+            delay_time = time.time() - _delay_start # take delay
+            play_obj.wait_done()
+            timed = time.time() - _from_sec
 
 
 if __name__ == "__main__":
